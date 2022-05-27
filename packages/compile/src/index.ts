96c037a36bef8d213fb4bcff794f5b8c3c77bad7
pinktu <chenbiuxiu@gmail.com>
2022-05-27 11:14:04 +0800

    feat: 导出compile模块中的函数

---
+++ packages/compile/src/index.ts
@@ -0,0 +1,6 @@
+import { setConfigPath } from '@/config'
+import { translate } from '@/gen'
+import { analyse } from '@/lexer'
+import { nodeParser } from '@/parser'
+
+export { setConfigPath, translate, analyse, nodeParser }
