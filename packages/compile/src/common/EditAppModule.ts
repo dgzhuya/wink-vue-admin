722d84f5421713caa8d245df1a2618ce5405a6a1
pinktu <chenbiuxiu@gmail.com>
2022-06-04 15:23:07 +0800

    feat: 优化nest app module文件操作

--- packages/compile/src/common/EditAppModule.ts
+++ packages/compile/src/common/EditAppModule.ts
@@ -1,51 +1,51 @@
 import { ObjectLiteralExpression, Project, PropertyAssignment } from 'ts-morph'
 import { getConfigPath } from '@/config'
 
-export const editAppModule = (moduleName: string, upperModuleName: string) => {
+export const editAppModule = (moduleName: string, upperModuleName: string, handleType: 'add' | 'remove' = 'add') => {
 	const path = getConfigPath().appModulePath
 	const project = new Project()
 	project.addSourceFileAtPath(path)
 	const sourceFile = project.getSourceFileOrThrow(path)
 
-	const appModuleClass = sourceFile.getClass('AppModule')
-	sourceFile.addImportDeclarations([
-		{
-			namedImports: [`${upperModuleName}Module`],
-			moduleSpecifier: `./${moduleName}/${moduleName}.module`
-		},
-		{
-			namedImports: [`${upperModuleName}Controller`],
-			moduleSpecifier: `./${moduleName}/${moduleName}.controller`
-		}
-	])
+	if (handleType === 'add') {
+		sourceFile.addImportDeclarations([
+			{
+				namedImports: [`${upperModuleName}Module`],
+				moduleSpecifier: `./${moduleName}/${moduleName}.module`
+			},
+			{
+				namedImports: [`${upperModuleName}Controller`],
+				moduleSpecifier: `./${moduleName}/${moduleName}.controller`
+			}
+		])
+	} else {
+		const moduleImport = sourceFile.getImportDeclarationOrThrow(`./${moduleName}/${moduleName}.module`)
+		moduleImport.remove()
+		const controller = sourceFile.getImportDeclarationOrThrow(`./${moduleName}/${moduleName}.controller`)
+		controller.remove()
+	}
 
-	if (appModuleClass) {
-		const decorator = appModuleClass.getDecorator('Module')
-		if (decorator) {
-			const callExpression = decorator.getCallExpression()
-			if (callExpression) {
-				const arg = callExpression.getArguments()[0]
-				if (arg) {
-					const imports = (arg as ObjectLiteralExpression).getProperty('imports')
-					if (imports) {
-						const propertyAssignment = imports as PropertyAssignment
-						const structure = propertyAssignment.getStructure()
-						if (structure) {
-							const initializer = structure.initializer.toString()
-							propertyAssignment.setInitializer(
-								`${initializer.slice(0, initializer.length - 1)}, ${upperModuleName}Module]`
-							)
-						}
-					}
-				}
-			}
-		}
-		const configureMethod = appModuleClass.getMethod('configure')
-		if (configureMethod) {
-			const bodyText = configureMethod.getBodyText()
-			if (bodyText) {
-				configureMethod.setBodyText(`${bodyText.slice(0, bodyText.length - 1)}, ${upperModuleName}Controller)`)
-			}
+	const appModuleClass = sourceFile.getClassOrThrow('AppModule')
+
+	const callExpression = appModuleClass.getDecoratorOrThrow('Module').getCallExpressionOrThrow()
+	const propertyAssignment = (callExpression.getArguments()[0] as ObjectLiteralExpression).getProperty(
+		'imports'
+	) as PropertyAssignment
+	const initializer = propertyAssignment.getStructure().initializer.toString()
+
+	if (handleType === 'add') {
+		propertyAssignment.setInitializer(`${initializer.slice(0, initializer.length - 1)}, ${upperModuleName}Module]`)
+	} else {
+		propertyAssignment.setInitializer(initializer.replace(`, ${upperModuleName}Module`, ''))
+	}
+
+	const configureMethod = appModuleClass.getMethodOrThrow('configure')
+	const bodyText = configureMethod.getBodyText()
+	if (bodyText) {
+		if (handleType === 'add') {
+			configureMethod.setBodyText(`${bodyText.slice(0, bodyText.length - 1)}, ${upperModuleName}Controller)`)
+		} else {
+			configureMethod.setBodyText(bodyText.replace(`, ${upperModuleName}Controller`, ''))
 		}
 	}
 
