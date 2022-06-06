def396945b4ed48e0183dfd72807ecf1f4cde229
pinktu <chenbiuxiu@gmail.com>
2022-06-06 21:36:44 +0800

    fix: 优化form文件生成

--- packages/compile/src/gen/vue/form/template.ts
+++ packages/compile/src/gen/vue/form/template.ts
@@ -1,127 +1,92 @@
 export const formSource = `<script lang="ts" setup>
-	import { %upperModuleName%Dto, %upperModuleName%Model } from '@/types/super-admin/%moduleName%'
-	import { update%upperModuleName%Info, create%upperModuleName%, set%upperModuleName%MajorRole } from '@/api/super-admin/%moduleName%'
+	import { PermissionDto, PermissionModel } from '@/types/super-admin/permission'
+	import { updatePermission, createPermission } from '@/api/super-admin/permission'
 	import { toast } from '@/utils/toast'
 
-	const props = defineProps<{ %moduleName%: %upperModuleName%Model | null; showForm: boolean }>()
-	const %moduleName%Info = ref<Partial<%upperModuleName%Dto>>({})
-	const createPassword = ref('')
-	const %moduleName%Id = ref(-1)
-	const reset%upperModuleName% = () => {
-		%moduleName%Id.value = -1
-		%moduleName%Info.value = {}
+	const props = defineProps<{ permission: PermissionModel | null; showForm: boolean }>()
+	const permissionId = ref(-1)
+	const permissionInfo = ref<Partial<PermissionDto>>({})
+	const resetPermission = () => {
+		permissionInfo.value = {}
+		permissionId.value = -1
 	}
 
 	watchEffect(async () => {
-		if (props.%moduleName% !== null) {
-			reset%upperModuleName%()
-			%moduleName%Id.value = props.%moduleName%.id
-			%moduleName%Info.value = {
-				...props.%moduleName%
+		if (props.permission !== null) {
+			resetPermission()
+			permissionId.value = props.permission.id
+			permissionInfo.value = {
+				title: props.permission.title,
+				description: props.permission.description,
+				key: props.permission.key
 			}
+			permissionId.value = props.permission.id
 		} else {
-			reset%upperModuleName%()
+			resetPermission()
 		}
 	})
 
-	const %moduleName%FormEmit = defineEmits<{ (e: 'close', refresh: boolean): void }>()
-
-	const uploadIcon = () => {
-		console.log('上传头像')
-	}
-
-	const setMajorHandler = async (rid: number) => {
-		if (props.%moduleName% !== null) await set%upperModuleName%MajorRole(props.%moduleName%.id, rid)
-	}
+	const permissionFormEmit = defineEmits<{ (e: 'close', refresh: boolean): void }>()
 
 	const closeHandler = () => {
-		%moduleName%FormEmit('close', false)
+		permissionFormEmit('close', false)
 	}
 
-	const update%upperModuleName% = async () => {
-		if (props.%moduleName% === null) {
-			await create%upperModuleName%({
-				...userInfo.value,
-				username: userInfo.value.username,
-				password: createPassword.value,
-				nickname: userInfo.value.nickname
-			})
+	const updateUser = async () => {
+		if (props.permission === null) {
+			%createCondition%			const createPermissionDto: PermissionDto = {
+				title: permissionInfo.value.title,
+				description: permissionInfo.value.description
+			}
+
+			if (permissionInfo.value.key !== '' && permissionInfo.value.key !== undefined) {
+				createPermissionDto.key = permissionInfo.value.key
+			}
+
+			if (props.parentId !== -1) {
+				createPermissionDto.parentId = props.parentId
+			}
+
+			await createPermission(createPermissionDto)
 		}
-		if (props.user) {
-			await update%upperModuleName%Info(userInfo.value, props.user.id)
+		if (props.permission) {
+			await updatePermission(permissionInfo.value, props.permission.id)
 		}
-		userFormEmit('close', true)
+		resetPermission()
+		permissionFormEmit('close', true)
 	}
 </script>
+
 <template>
-<el-dialog
-	:model-value="showForm"
-	:title="userId === -1 ? '添加用户' : '编辑用户'"
-	width="50%"
-	@close="closeHandler()"
-	custom-class="user-form-container"
-	lock-scroll
->
-	<el-form ref="formRef" :model="userInfo" label-width="120px" class="user-detail">
-		<el-form-item label="用户名">
-			<el-input v-model="userInfo.username" />
-		</el-form-item>
-		<el-form-item v-if="user === null" label="密码">
-			<el-input v-model="createPassword" />
-		</el-form-item>
-		<el-form-item label="昵称">
-			<el-input v-model="userInfo.nickname" />
-		</el-form-item>
-		<el-form-item v-if="user !== null && user.roles && user.roles.length > 0" label="主要角色">
-			<el-select v-model="user.majorId" @change="setMajorHandler" placeholder="未知">
-				<el-option v-for="role in user.roles" :value="role.id" :label="role.title" :key="role.id" />
-			</el-select>
-		</el-form-item>
-		<el-form-item label="性别">
-			<el-select v-model="userInfo.gender" placeholder="未知">
-				<el-option label="女" :value="0" />
-				<el-option label="男" :value="1" />
-			</el-select>
-		</el-form-item>
-		<el-form-item label="手机号">
-			<el-input v-model="userInfo.mobile" />
-		</el-form-item>
-		<el-form-item label="地址">
-			<el-input v-model="userInfo.address" maxlength="100" show-word-limit type="textarea" />
-		</el-form-item>
-	</el-form>
-	<template #footer>
-		<div class="user-form-footer">
-			<el-button @click="update%upperModuleName%()" type="primary">保存</el-button>
-			<el-button @click="closeHandler()" type="info">取消</el-button>
-		</div>
-	</template>
-</el-dialog>
+	<el-dialog
+		:model-value="showForm"
+		:title="props.permission === null ? '添加权限' : '编辑权限'"
+		width="50%"
+		@close="closeHandler()"
+		custom-class="form-container"
+		lock-scroll
+	>
+		<el-form ref="formRef" :model="permissionInfo" label-width="120px" class="form-detail">
+			<el-form-item label="标题">
+				<el-input v-model="permissionInfo.title" />
+			</el-form-item>
+			<el-form-item label="关键字">
+				<el-input v-model="permissionInfo.key" />
+			</el-form-item>
+			<el-form-item label="描述">
+				<el-input v-model="permissionInfo.description" maxlength="100" show-word-limit type="textarea" />
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<div class="form-footer">
+				<el-button @click="updateUser()" type="primary">保存</el-button>
+				<el-button @click="closeHandler()" type="info">取消</el-button>
+			</div>
+		</template>
+	</el-dialog>
 </template>
+
 <style lang="scss" scoped>
-.%moduleName%-form-container {
-	position: relative;
-}
-.%moduleName%-detail {
-	text-align: center;
-	width: 70%;
-	margin: 0 auto;
-
-	.%moduleName%-avatar {
-		cursor: pointer;
-		width: 50px;
-		height: 50px;
-		box-sizing: border-box;
-		border-radius: 10px;
-		font-size: 50px;
-		margin-bottom: 10px;
-	}
-}
-
-.%moduleName%-form-footer {
-	width: 100%;
-	display: flex;
-	justify-content: center;
-}
+	@import '@/style/form.scss';
 </style>
 `
