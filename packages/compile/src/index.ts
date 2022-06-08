aee9ed4382a27c82e08044e792b0aec95f43b4b6
pinktu <chenbiuxiu@gmail.com>
2022-06-08 09:27:50 +0800

    feat: 添加文件编辑函数导出

--- packages/compile/src/index.ts
+++ packages/compile/src/index.ts
@@ -3,8 +3,14 @@ import { translate } from '@/gen'
 import { analyse } from '@/lexer'
 import { nodeParser } from '@/parser'
 import { getModuleDescription, getModuleName, getModuleComment, getRouterInfo } from '@/gen/util/getInfoByAST'
+import { editAppModule } from './common/EditAppModule'
+import { editAsyncRoute } from './common/EditAsyncRoute'
+import { editVueRouter } from './common/EditRouter'
 
 export {
+	editAppModule,
+	editAsyncRoute,
+	editVueRouter,
 	setConfigPath,
 	translate,
 	analyse,
