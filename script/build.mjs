54ca4cdba6c0e641d1a90bc60ec73783b99ff9a9
pinktu <chenbiuxiu@gmail.com>
2022-06-10 00:15:00 +0800

    fix: 服务器重启于前端页面关闭前

--- script/build.mjs
+++ script/build.mjs
@@ -2,6 +2,6 @@ import shell from 'shelljs'
 
 shell.cd('./packages/api')
 shell.exec('npm run build')
-shell.exec('pm2 restart wink-api')
 shell.cd('../web')
 shell.exec('npm run build:prod')
+shell.exec('pm2 restart wink-api')
