dafec21e5a267a5aabf4cea6251871ba3a1310a2
pinktu <chenbiuxiu@gmail.com>
2022-06-10 17:32:29 +0800

    feat: 添加前端文件清理功能

--- packages/build/src/index.mjs
+++ packages/build/src/index.mjs
@@ -1,6 +1,6 @@
 import { WebSocketServer } from 'ws'
 import shell from 'shelljs'
-import path from 'path'
+import fs from 'fs'
 
 const wss = new WebSocketServer({ port: 9527 })
 
@@ -9,12 +9,20 @@ wss.on('connection', (ws, request) => {
 	if (codePath !== '' && request.url === `/build/${codePath}`) {
 		codePath = ''
 		ws.send(JSON.stringify({ code: 200, msg: '开始编译API' }))
-		console.log(path.resolve(path.dirname(''), ''))
 		shell.cd('../api')
 		shell.exec('npm run build')
 		ws.send(JSON.stringify({ code: 200, msg: '开始编译web' }))
 		shell.cd('../web')
 		shell.exec('npm run build:prod')
+		if (fs.existsSync('/var/www/admin_web')) {
+			shell.rm(
+				'-rf',
+				'/var/www/admin_web/assets',
+				'/var/www/admin_web/favicon.ico',
+				'/var/www/admin_web/index.html'
+			)
+			shell.cp('-r', './dist/*', '/var/www/admin_web/')
+		}
 		ws.send(JSON.stringify({ code: 200, msg: '项目编译成功,开始重启' }))
 		shell.exec('pm2 restart wink-api')
 		ws.send(JSON.stringify({ code: 201, msg: '项目重启成功' }))
