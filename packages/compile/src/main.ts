d0e7c26f7e08cd7c728b28b81c5eb86b2a60609a
pinktu <chenbiuxiu@gmail.com>
2022-06-11 10:17:21 +0800

    fix: 修改compile main文件位置

--- packages/compile/src/main.ts
+++ packages/compile/src/main.ts
@@ -3,7 +3,7 @@ import { analyse, nodeParser, setConfigPath, clearModule } from 'dist'
 
 setConfigPath({ nestDir: '../api/src/', webDir: '../web/src/' })
 
-const source = readFileSync('/Users/pinktu/Desktop/wink-scripts/field.wks').toString()
+const source = readFileSync('../../sources/model.wks').toString()
 const { data, error } = analyse(source)
 if (!error) {
 	nodeParser(data).then(res => {
