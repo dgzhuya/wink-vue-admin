a06551f268bd161d65bc11442e8afb16e6f0bba8
pinktu <chenbiuxiu@gmail.com>
2022-06-11 10:48:37 +0800

    fix: 添加websocket wss连接功能

--- packages/web/src/utils/webSocket.ts
+++ packages/web/src/utils/webSocket.ts
@@ -1,5 +1,6 @@
 export const webScoket = (wsPath: string) => {
-	const ws = new WebSocket(`ws://${location.host}${import.meta.env.VITE_BASE_WS}/build/${wsPath}`)
+	const wsProtocol = document.location.protocol === 'https:' ? 's' : ''
+	const ws = new WebSocket(`ws${wsProtocol}://${location.host}${import.meta.env.VITE_BASE_WS}/build/${wsPath}`)
 	ws.addEventListener('message', event => {
 		console.log('Message from server ', event.data)
 	})
