8b1e2f8f02e812c16b35fcb0e3a2e91da1fb8532
pinktu <chenbiuxiu@gmail.com>
2022-06-25 22:34:31 +0800

    feat: 添加获取编译配置信息导出

--- packages/compile/src/index.ts
+++ packages/compile/src/index.ts
@@ -1,10 +1,11 @@
-import { setConfigPath } from '@/config'
+import { setConfigPath, getConfigPath } from '@/config'
 import { translate, clearModule } from '@/gen'
 import { analyse } from '@/lexer'
 import { nodeParser } from '@/parser'
 import { getModuleDescription, getModuleName, getModuleComment, getRouterInfo } from '@/gen/util/getInfoByAST'
 
 export {
+	getConfigPath,
 	setConfigPath,
 	translate,
 	analyse,
