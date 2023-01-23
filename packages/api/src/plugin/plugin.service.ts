06c79285257b54d9f416afb1039a3d37c4f7c85b
dgzhuya <chenbiuxiu@gmail.com>
2023-01-23 12:34:57 +0800

    🐞 fix: 编译bug修复

--- packages/api/src/plugin/plugin.service.ts
+++ packages/api/src/plugin/plugin.service.ts
@@ -15,7 +15,7 @@ import {
 	translate,
 	getModuleComment,
 	clearModule
-} from 'wink-creater'
+} from '@biuxiu/compile'
 import { BadParamsException } from '@/common/exception/bad-params-exception'
 import { DefaultPluginInfo } from '@/config/plugin'
 import { Permission } from '@/permission/entities/permission.entity'
