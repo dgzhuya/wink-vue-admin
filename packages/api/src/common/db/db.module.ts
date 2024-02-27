e5eb9786f08bf43c5eece83f90c82902d376d67f
dgzhuya <chenbiuxiu@gmail.com>
2024-02-27 17:39:35 +0800

    🔧 build: 升级后端依赖

--- packages/api/src/common/db/db.module.ts
+++ packages/api/src/common/db/db.module.ts
@@ -4,7 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
-			type: 'sqlite',
+			type: 'better-sqlite3',
 			database: 'admin.db',
 			autoLoadEntities: true,
 			synchronize: true
