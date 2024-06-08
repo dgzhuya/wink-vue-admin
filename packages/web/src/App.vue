c66ae63a03822bbe230cf688ca146810d6e138fb
dgzhuya <chenbiuxiu@gmail.com>
2024-06-08 17:06:24 +0800

    🦄 refactor: 添加element-plus汉化

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-	<el-config-provider>
+	<el-config-provider :locale="zhCn">
 		<router-view></router-view>
 	</el-config-provider>
 </template>
 <script lang="ts" setup>
 	import { generateNewStyle, writeStyle } from '@web/utils/style'
+	// @ts-ignore
+	import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 	import { useTheme } from './store/module/theme'
 
 	const { color } = useTheme()
