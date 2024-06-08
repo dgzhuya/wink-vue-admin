d87c54dbc5bd0e61ec54cc5cd79a5aee6de6bdb5
dgzhuya <chenbiuxiu@gmail.com>
2024-06-08 11:52:51 +0800

    🦄 refactor: 清理语言信息

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,14 +1,12 @@
 <template>
-	<el-config-provider :locale="locale">
+	<el-config-provider>
 		<router-view></router-view>
 	</el-config-provider>
 </template>
 <script lang="ts" setup>
-	import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 	import { generateNewStyle, writeStyle } from '@web/utils/style'
 	import { useTheme } from './store/module/theme'
 
-	const locale = zhCn
 	const { color } = useTheme()
 	generateNewStyle(color).then(styleText => writeStyle(styleText))
 </script>
