aa203ff97159df582dfb3f6c206d00891684ed52
dgzhuya <chenbiuxiu@gmail.com>
2024-06-17 16:52:02 +0800

    🐳 chore: 修改pm2部署

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,17 +9,7 @@ module.exports = {
 			script: 'dist/main.js',
 			cwd: path.resolve(__dirname, 'packages/api/'),
 			env: {
-				NODE_ENV: 'production',
-				WS_KEY: 'ws_key'
-			}
-		},
-		{
-			name: 'wink-build',
-			script: 'src/index.mjs',
-			cwd: path.resolve(__dirname, 'packages/build/'),
-			env: {
-				NODE_ENV: 'production',
-				WS_KEY: 'ws_key'
+				NODE_ENV: 'production'
 			}
 		}
 	]
