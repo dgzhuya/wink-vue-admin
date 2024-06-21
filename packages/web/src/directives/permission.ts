be1c0bafd3c0f342da000af5282ad4516a525868
dgzhuya <chenbiuxiu@gmail.com>
2024-06-21 18:31:33 +0800

    🐞 fix: 指令多次被调用

--- packages/web/src/directives/permission.ts
+++ packages/web/src/directives/permission.ts
@@ -6,14 +6,16 @@ import router from '@web/router'
 
 export default (app: App<Element>) => {
 	const user = useUser()
-	app.directive('permission', (el: HTMLElement, binging: DirectiveBinding<PermissionPageType[]>) => {
-		const { path } = router.currentRoute.value
-		const pKey = formatPermissionKey(path)
-		const keys = binging.value.map(v => `${pKey}@${v}`)
-		const permission = user.getPermissions
-		const hasPermission = keys.some(k => permission.includes(k))
-		if (!hasPermission) {
-			el.parentNode && el.parentNode.removeChild(el)
+	app.directive('permission', {
+		mounted: (el: HTMLElement, { value }: DirectiveBinding<PermissionPageType[]>) => {
+			const { path } = router.currentRoute.value
+			const pKey = formatPermissionKey(path)
+			const permission = user.getPermissions
+			const keys = value.map(v => `${pKey}@${v}`)
+			const hasPermission = keys.some(k => permission.includes(k))
+			if (!hasPermission) {
+				el.parentNode && el.parentNode.removeChild(el)
+			}
 		}
 	})
 }
