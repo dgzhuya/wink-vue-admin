40f77b54dda16aab9cdd70b958791b6ebd9a2ad6
dgzhuya <chenbiuxiu@gmail.com>
2024-06-22 16:54:02 +0800

    ✨ feat: 实现路由自动加载

--- packages/web/src/router/index.ts
+++ packages/web/src/router/index.ts
@@ -1,3 +1,4 @@
+import { MetaStruct } from '@web/types'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
@@ -42,9 +43,59 @@ const routes: RouteRecordRaw[] = [
 	}
 ]
 
+const metaInfo = import.meta.glob('../modules/**/meta.json', { import: 'default', eager: true })
+
+const mataList: MetaStruct[] = Object.entries(metaInfo).map(([path, m]) => {
+	const meta = m as MetaStruct
+	meta.filePath = path.replace('meta.json', '') + 'index.vue'
+	return meta
+})
+const map: Record<string, MetaStruct[]> = {}
+for (let i = 0; i < mataList.length; i++) {
+	const route = mataList[i]
+	const pathList = route.path.split('/')
+	pathList.pop()
+	const parentPath = pathList.join('/') || '/'
+	if (map[parentPath]) {
+		map[parentPath].push(route)
+	} else {
+		map[parentPath] = [route]
+	}
+}
+const pages = import.meta.glob('../modules/**/index.vue')
+const autoRoutes: RouteRecordRaw[] = map['/'].map(({ path, title, filePath, icon, redirect }) => {
+	const children = map[path]
+	const baseRoute: RouteRecordRaw = {
+		path: children ? path : '/',
+		component: () => import('@web/layout/index.vue'),
+		children: children
+			? children.map(({ path, filePath, title, icon }) => {
+					return {
+						path,
+						meta: { title, icon },
+						component: pages[filePath]
+					}
+				})
+			: [
+					{
+						path,
+						meta: { title, icon },
+						component: pages[filePath]
+					}
+				]
+	}
+	if (children) {
+		baseRoute['meta'] = { title, icon }
+		baseRoute['redirect'] = redirect ? `${path}/${redirect}` : children[0].path
+	}
+	return baseRoute
+})
+
 const router = createRouter({
 	history: createWebHistory(),
 	routes
 })
-
+autoRoutes.forEach(route => {
+	router.addRoute(route)
+})
 export default router
