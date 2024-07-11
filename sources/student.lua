21de228d5e691a7e46a1c4bceba60d3ae1b18f27
dgzhuya <chenbiuxiu@gmail.com>
2024-07-11 17:20:23 +0800

    ✨ feat: 添加代码生成测试模板

--- sources/student.lua
+++ sources/student.lua
@@ -1,19 +1,29 @@
-Nest.render({ name = 'student', path = 'test/' }, {
-	Nest.entity('name', 'string', Nest.cType('varchar'),
-		Nest.column('length', 100), Nest.column('comment', '学生姓名')),
-	Nest.entity('score', 'number', Nest.column('comment', '学生成绩'),
-		Nest.cType('int'), Nest.column('nullable', true)),
-	Nest.entity('age', 'number', Nest.column('name', 'age_s'),
-		Nest.cType('int'), Nest.column('comment', '学生年龄'))
-}, {
-	Nest.dto('name', 'string', Nest.rule('isOptional', true),
-		Nest.limit("姓名长度在2-100之间", 2, 100)),
-	Nest.dto('score', 'number', Nest.rule('notEmpty', '成绩不能为空'),
-		Nest.limit("成绩在0-100之间", 0, 100)),
-	Nest.dto('age', 'number', Nest.rule('isInt', '年龄应该为数字'),
-		Nest.limit("年龄应该小于100岁", nil, 100)
-	)
-}, {
-	Nest.service('update'), Nest.service('add'), Nest.service('get'),
-	Nest.service('all'), Nest.service('delete')
-})
+local nameField = FieldNew('name', 'string', '姓名')
+local scoreField = FieldNew('score', 'number', '成绩')
+local ageField = FieldNew('age', 'number', '年龄')
+
+local entity = {
+	ORM.new(nameField, ORM.cType('varchar'), ORM.column('length', 100)),
+	ORM.new(scoreField, ORM.cType('int'), ORM.column('nullable', true)),
+	ORM.new(ageField, ORM.cType('int'))
+}
+
+local dto = {
+	DTO.new(nameField, DTO.rule('isOptional', true), DTO.limit("姓名长度在1-10之间", 1, 10)),
+	DTO.new(scoreField, DTO.rule('notEmpty', '成绩不能为空'), DTO.limit("成绩在0-100之间", 0, 100)),
+	DTO.new(ageField, DTO.rule('isInt', '年龄应该为数字'), DTO.limit("年龄应该小于100岁", nil, 100))
+}
+
+local service = {
+	API.new('update'), API.new('get', true, true), API.new('delete'), API.new('add'), API.new('all')
+}
+
+local config = {
+	name = 'student', comment = '学生'
+}
+NestRender(config, entity, dto, service)
+VueRender(config, {
+	path = '/student',
+	icon = 'student',
+	title = '学生信息'
+}, { nameField, scoreField, ageField }, service)
